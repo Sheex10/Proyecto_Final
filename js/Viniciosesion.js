9c3f3942c4708547a77cf8dd679c17db99afd073
Patreyesa <pat.reyesa@duocuc.cl>
2023-05-01 17:32:53 -0400

    Validacion inicio sesion
    Validacion inicio sesion

---
+++ js/Viniciosesion.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+    $("#loginform").submit(function(e){
+        e.preventDefault();
+        var nombre = $("#nombre").val();
+        var contrasena = $("#clave").val();
+
+        let mensaje="";
+        let enviar=false;
+
+        if(nombre.trim().length < 4 || nombre.trim().length > 10){
+            mensaje += "El nombre de usuario debe tener al menos 4 a 10 caracteres";
+            enviar = true;
+        }
+
+        var letra=nombre.charAt(0);
+        if(!esMayuscula(letra)){
+            mensaje+= "<br>El primer caracter debe ser mayuscula";
+            enviar=true;
+        }
+        //validar password
+        if(contrasena.trim().length < 8 || contrasena.trim().length > 12){
+            mensaje = mensaje + "<br>Debe tener entre 8 y 12 caracteres.";
+            enviar = true;
+        }
+
+        if(contrasena.trim()  == ""){
+            mensaje += "<br>No puede estar vacia.";
+            enviar = true;
+        }
+
+        if (!contrasena.match(/([A-Z])/)){
+           mensaje += "<br>Debe contener al menos una mayuscula y una minuscula.";
+            enviar = true;
+        }
+
+         if (!contrasena.match(/([0-9])/)){
+            mensaje += "<br>Debe contener al menos un digito numerico.";
+            enviar = true;
+         }
+
+        if (!contrasena.match(/([@,#,.,,])/)){
+            mensaje += "<br>Debe contener un simbolo o caracter especial (@,#,.,,)";
+            enviar = true;
+         }
+
+
+
+
+
+
+        
+        if(enviar){
+            $("#warnings").html(mensaje);
+        }
+        else{
+            $("#warnings").html("Sesion iniciada");
+        }
+
+    });
+
+    function esMayuscula(letra){
+        console.log(letra);
+        console.log(letra.toUpperCase());
+        if(letra == letra.toUpperCase()){
+            return true;
+        }
+        else{
+            return false;
+        }
+         
+    }
+
+
+});
